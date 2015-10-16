9e10f19286df6a780221688ebf5fc38a24629b70
Javier García Hernández <javier.garcia@atos.net>
2015-10-16 15:36:18 +0200

    modifications

--- docker/deploy_aeon.sh
+++ docker/deploy_aeon.sh
@@ -5,7 +5,9 @@ git clone -b docker https://github.com/atos-ari-aeon/fiware-cloud-messaging-api.
 git clone -b docker https://github.com/atos-ari-aeon/fiware-cloud-messaging-events-manager.git
 
 sudo apt-get update
-sudo apt-get install docker.io curl python-pip -y
+sudo apt-get install wget curl python-pip -y
+
+wget -qO- https://get.docker.com/ | sh
 
 pip install -U docker-compose
 
