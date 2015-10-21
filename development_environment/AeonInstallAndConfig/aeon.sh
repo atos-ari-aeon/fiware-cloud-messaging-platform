b47afcd721e65c264463df1762ead1159cc35cf9
Javier García Hernández <javier.garcia@atos.net>
2015-10-21 16:28:10 +0200

    script improvement. No tgz required

--- development_environment/AeonInstallAndConfig/aeon.sh
+++ development_environment/AeonInstallAndConfig/aeon.sh
@@ -4,26 +4,38 @@
 
  # create AEON home
  sudo mkdir -p /home/aeon
-
- # copy AEON sources to home
- cp aeon.tgz /home/aeon/
- cd /home/aeon
- pwd
-
- sudo tar -xzf aeon.tgz 
-
- ls -ltr
  
+ cd /home/aeon
+ 
+ echo "*******************************"
+ echo "*   Configuring Environment   *"
+ echo "*******************************"
  # Install puppet
  sudo apt-get update
- sudo apt-get install puppet -y
+ sudo apt-get install puppet git -y
 
- sudo apt-get -y install build-essential
+ echo "************************************************"
+ echo "*   Downloading AEON Cloud Messaging modules   *"
+ echo "************************************************"
+
+ # Download the code from the repos
+ git clone https://github.com/atos-ari-aeon/fiware-cloud-messaging-platform.git
+ git clone https://github.com/atos-ari-aeon/fiware-cloud-messaging-dashboard.git
+ git clone https://github.com/atos-ari-aeon/fiware-cloud-messaging-api.git
+ git clone https://github.com/atos-ari-aeon/fiware-cloud-messaging-events-manager.git
+ 
+ echo "*******************************"
+ echo "*   Configuring Environment   *"
+ echo "*******************************"
+
+ # Get the installation and config files
+ mv ./fiware-cloud-messaging-platform/development_environment/AeonInstallAndConfig/aeonInstall .
+ mv ./fiware-cloud-messaging-platform/development_environment/AeonInstallAndConfig/aeonConfiguration .
 
  # install AEON
  sudo puppet apply --modulepath /home/aeon/aeonInstall/modules /home/aeon/aeonInstall/manifests/main.pp
 
- # install Forever
- sudo npm install forever --global
+ echo "Cleaning temporal files..."
 
-
+ # Delete useless files
+ rm -rf ./fiware-cloud-messaging-platform
